413e67d7691ac13d898de20ef46e94cf08fb1130
Florencia Gadea <florencia@rotterdam-cs.com>
2012-12-13 13:29:17 -0300

    added new methods

--- docroot/WEB-INF/src/com/rcs/service/service/impl/MessageSourceLocalServiceImpl.java
+++ docroot/WEB-INF/src/com/rcs/service/service/impl/MessageSourceLocalServiceImpl.java
@@ -17,20 +17,14 @@ package com.rcs.service.service.impl;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
-
-import sun.util.logging.resources.logging;
 
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
@@ -41,16 +35,13 @@ import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
-import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.service.PersistedModelLocalServiceRegistryUtil;
-import com.rcs.common.utils.CustomMessage;
+import com.rcs.common.utils.Importer;
 import com.rcs.common.utils.MessageSourceWrapper;
 import com.rcs.common.utils.RcsConstants;
 import com.rcs.service.model.MessageSource;
 import com.rcs.service.service.MessageSourceLocalServiceUtil;
 import com.rcs.service.service.base.MessageSourceLocalServiceBaseImpl;
-import com.rcs.service.service.persistence.CustomMessageSourceLocalServiceUtil;
-import com.rcs.service.service.persistence.MessageSourceUtil;
 
 /**
  * The implementation of the message source local service.
@@ -84,21 +75,10 @@ public class MessageSourceLocalServiceImpl
 		List<String> results = (List<String>) MessageSourceLocalServiceUtil.dynamicQuery(query);		
 		return results;
 	}
-	//@@ borrar esto, seguro anda mal
-	/*
-	public long getMessageSourcesKeyCount(int start, int end) throws SystemException {
-		DynamicQuery query = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		query.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.key")));
-		query.setLimit(start, end);
-		long count = MessageSourceLocalServiceUtil.dynamicQueryCount(query);		
-		return count;
-	}*/
 	
 	public long getMessageSourcesKeyCount() throws SystemException {
-		DynamicQuery query = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		//query.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.resourceKey")));
-		query.setProjection(ProjectionFactoryUtil.countDistinct("primaryKey.key"));		
-		//long count = MessageSourceLocalServiceUtil.dynamicQueryCount(query);		
+		DynamicQuery query = DynamicQueryFactoryUtil.forClass(MessageSource.class);		
+		query.setProjection(ProjectionFactoryUtil.countDistinct("primaryKey.key"));				
 		List result = MessageSourceLocalServiceUtil.dynamicQuery(query);
 		Long count = (Long) result.get(0);
 		return count.longValue();
@@ -114,74 +94,18 @@ public class MessageSourceLocalServiceImpl
 	
 	public List<MessageSource> getMessageSources(int start, int end) throws SystemException {
 		List <String> keys = this.getMessageSourcesKey(start, end);
-		/*DynamicQuery subQuery = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		subQuery.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.resourceKey")));
-		subQuery.setLimit(start, end);*/
-		//@@ probar subquery
-		//@@ borrar clases del custom query
-		DynamicQuery mainQuery = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		mainQuery.add(PropertyFactoryUtil.forName("primaryKey.key").in(keys.toArray()));
-		mainQuery.addOrder(OrderFactoryUtil.asc("primaryKey.key"));
-		List<MessageSource> results = (List<MessageSource>) MessageSourceLocalServiceUtil.dynamicQuery(mainQuery);
-		return results;
-	}
-	
-	/*
-	public List<MessageSource> getMessageSources(int start, int end) throws SystemException {
-		DynamicQuery query = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		//query.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.resourceKey")));
-		/*query.setProjection(ProjectionFactoryUtil.groupProperty("primaryKey.resourceKey"));
-		List<MessageSource> results = (List<MessageSource>) MessageSourceLocalServiceUtil.dynamicQuery(query, start, end);;*/
-	/*	query.add(PropertyFactoryUtil.forName("primaryKey.resourceLocale").eq("en_GB"));		
-		List<MessageSource> results = (List<MessageSource>) CustomMessageSourceLocalServiceUtil.dynamicQuery(query, start, end);
-		//getDefaultLocale
-		//_logger.error("genericResult: " + results);
-		
-		/*query = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		query.add(PropertyFactoryUtil.forName("primaryKey.resourceKey").in(arg0)*/
-				
-		//List<MessageSource> results = new ArrayList<MessageSource>();
-		//List<MessageSource> results = (List<MessageSource>) (List) genericResult;
-		//results.addAll(genericResult);
-	/*	return results;
-	}*/
-	/*
-	public List<MessageSource> getMessageSources(int start, int end) throws SystemException {
-		return CustomMessageSourceLocalServiceUtil.getMessageSources(start, end);
-    }*/
-	
-	public List<MessageSource> getMessageSources(String resourceKey, String resourceLocale, String resourceValue, String resourceBundle, int start, int end) throws SystemException {
-		/*DynamicQuery query = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		if(StringUtils.isNotBlank(resourceKey)) {		
-			query.add(RestrictionsFactoryUtil.ilike("primaryKey.key", "%" + resourceKey + "%"));			
-		}		
-		if(StringUtils.isNotBlank(resourceLocale)) {		
-			query.add(PropertyFactoryUtil.forName("primaryKey.locale").eq(resourceLocale));
-		}
-		if(StringUtils.isNotBlank(resourceValue)) {
-			query.add(RestrictionsFactoryUtil.ilike("value", "%" + resourceValue + "%")); //forName("resourceValue").ilike("%" + resourceValue + "%"));			
-		}
-		if(StringUtils.isNotBlank(resourceBundle)) {		
-			query.add(PropertyFactoryUtil.forName("bundle").eq(resourceBundle));
-		}					
-		query.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.key")));
-		query.add(PropertyFactoryUtil.forName("primaryKey.locale").isNotNull());
-		System.out.println("query.toString(): " + query.toString());
-		List genericResult = MessageSourceLocalServiceUtil.dynamicQuery(query, start, end);;		
 		List<MessageSource> results = new ArrayList<MessageSource>();
-		results.addAll(genericResult);		 	
-		return results;*/
-		_logger.error("resourceKey: " + resourceKey);
-		_logger.error("resourceLocale: " + resourceLocale);
-		_logger.error("resourceValue: " + resourceValue);
-		_logger.error("resourceBundle: " + resourceBundle);
+		if(keys.size() != 0) {
+			DynamicQuery mainQuery = DynamicQueryFactoryUtil.forClass(MessageSource.class);
+			mainQuery.add(PropertyFactoryUtil.forName("primaryKey.key").in(keys.toArray()));
+			mainQuery.addOrder(OrderFactoryUtil.asc("primaryKey.key"));
+			results = (List<MessageSource>) MessageSourceLocalServiceUtil.dynamicQuery(mainQuery);
+		} 		
+		return results;
+	}	
+	
+	public List<MessageSource> getMessageSources(String resourceKey, String resourceLocale, String resourceValue, String resourceBundle, int start, int end) throws SystemException {		
 		List <String> keys = this.getMessageSourcesKey(resourceKey, resourceLocale, resourceValue, resourceBundle, start, end);
-		/*DynamicQuery subQuery = DynamicQueryFactoryUtil.forClass(MessageSource.class);
-		subQuery.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.resourceKey")));
-		subQuery.setLimit(start, end);*/
-		//@@ probar subquery
-		//@@ borrar clases del custom query
-		_logger.error("keys: " + keys); //@@ chequear que no esté vacío
 		if(keys.size() == 0) {
 			return new ArrayList<MessageSource>();
 		}
@@ -200,13 +124,12 @@ public class MessageSourceLocalServiceImpl
 		if(StringUtils.isNotBlank(resourceLocale)) {		
 			query.add(PropertyFactoryUtil.forName("primaryKey.locale").eq(resourceLocale));
 		}
-		if(StringUtils.isNotBlank(resourceValue)) {
-			query.add(RestrictionsFactoryUtil.ilike("value", "%" + resourceValue + "%")); //forName("resourceValue").ilike("%" + resourceValue + "%"));			
-		}
-		if(StringUtils.isNotBlank(resourceBundle) && !RcsConstants.ALL_BUNDLES.equals(resourceBundle)) {
-			_logger.error("RcsConstants.ALL_BUNDLES.equals(resourceBundle): " + RcsConstants.ALL_BUNDLES.equals(resourceBundle));
+		if(StringUtils.isNotBlank(resourceBundle) && !RcsConstants.ALL_BUNDLES.equals(resourceBundle)) {			
 			query.add(PropertyFactoryUtil.forName("bundle").eq(resourceBundle));
 		}
+		if(StringUtils.isNotBlank(resourceValue)) {
+			query.add(RestrictionsFactoryUtil.ilike("value", "%" + resourceValue + "%")); //forName("resourceValue").ilike("%" + resourceValue + "%"));			
+		}		
 		query.setProjection(ProjectionFactoryUtil.distinct(ProjectionFactoryUtil.property("primaryKey.key")));
 		query.addOrder(OrderFactoryUtil.asc("primaryKey.key"));
 		List<String> results = (List<String>) MessageSourceLocalServiceUtil.dynamicQuery(query, start, end);		
@@ -230,7 +153,6 @@ public class MessageSourceLocalServiceImpl
 		query.setProjection(ProjectionFactoryUtil.countDistinct("primaryKey.key"));									
 		List result = MessageSourceLocalServiceUtil.dynamicQuery(query);
 		Long count = (Long) result.get(0);			
-		_logger.error("getMessageSourcesKeyCount: " + count.longValue());
 		return count.longValue();
 	}
 	
@@ -238,9 +160,8 @@ public class MessageSourceLocalServiceImpl
 
         List<MessageSourceWrapper> messageSourceWrapperList = new LinkedList<MessageSourceWrapper>();
 
-        MessageSourceWrapper messageSourceWrapper = null;// = new MessageSourceWrapper();
+        MessageSourceWrapper messageSourceWrapper = null;
         String preKey = null;
-        //_logger.error("messageSourceList: " + messageSourceList);
         for (MessageSource messageSource : messageSourceList) {
             if (!StringUtils.equals(preKey, messageSource.getKey())) {
                 messageSourceWrapper = new MessageSourceWrapper();
@@ -262,22 +183,11 @@ public class MessageSourceLocalServiceImpl
 
             return getMSWrappersJSON(messageSourceWrapperList);
         } catch (Exception e) {
-            _logger.error("Cannot get resource cause: " + e.getMessage(), e);
+            _logger.warn("Cannot get resource cause: " + e.getMessage(), e);
         }
         
         return EMPTY_JSON;
     }					 
-    
-	/*@@problems
-    public String getCMJson(List<CustomMessage> customMessages) {
-        try {
-            return new ObjectMapper().writeValueAsString(customMessages);
-        } catch (IOException e) {
-            //_logger.error("Cannot serialize CustomMessage list, cause: " + e.getMessage(), e);
-        }
-        return EMPTY_JSON;
-        //return "";
-    }*/
     
     private String getMSWrappersJSON(List<MessageSourceWrapper> messageSourceWrapperList) {
         ObjectMapper om = new ObjectMapper();
@@ -356,25 +266,11 @@ public class MessageSourceLocalServiceImpl
 
             return getMSWrappersJSON(messageSourceWrapperList);
         } catch (Exception e) {
-            _logger.error("Cannot get resource cause: " + e.getMessage(), e);
+            _logger.warn("Cannot get resource cause: " + e.getMessage(), e);
         }
         return EMPTY_JSON;
     }
-	/*
-	public String getMSWJson(String key, String value, String locale, String bundle, int start, int end) {
-        return new StringBuilder()
-                .append("{\"totalRecords\":\"")
-                .append(findMessageSourceListCount(key, value, locale, bundle))
-                .append("\",")
-                .append("\"start\":\"")
-                .append(start)
-                .append("\",")
-                .append("\"records\":")
-                .append(getMessageSourceWrappers(key, value, locale, bundle, start, end))
-                .append("}")
-                .toString();
-    }*/
-    
+	    
     public List<MessageSource> getMessageSourcesByKey(String key) throws PortalException, SystemException {	    
 	    List<MessageSource> entities = messageSourcePersistence.findByKey(key);
 		return entities;
@@ -384,13 +280,11 @@ public class MessageSourceLocalServiceImpl
     	messageSourcePersistence.clearCache();
     }
     
-    @Override
-    public void afterPropertiesSet() {
-		PersistedModelLocalServiceRegistryUtil.register("com.rcs.service.model.MessageSource",
-			messageSourceLocalService);
-		try {
-			messageSourcePersistence.findAll();
-		} catch(SystemException e) {			
-		}		
-	}
+    public List<MessageSource> findAll() throws SystemException {
+    	return messageSourcePersistence.findAll();
+    }
+    
+    public List<MessageSource> findByBundle(String bundle) throws SystemException {
+    	return messageSourcePersistence.findByBundle(bundle);
+    }
 }
