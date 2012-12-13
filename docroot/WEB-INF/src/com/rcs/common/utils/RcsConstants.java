22f62aea02510076118baba5486170165ab6d3a5
Florencia Gadea <florencia@rotterdam-cs.com>
2012-12-13 13:31:04 -0300

    added default constant

--- docroot/WEB-INF/src/com/rcs/common/utils/RcsConstants.java
+++ docroot/WEB-INF/src/com/rcs/common/utils/RcsConstants.java
@@ -4,7 +4,7 @@ package com.rcs.common.utils;
 public interface RcsConstants {
 
     String DEFAULT_BUNDLE_NAME  = "Default";
-
+    String I18N_EDITOR_BUNDLE_NAME  = "I18N Editor Portlet";
     String ALL_BUNDLES          = "all";
 
 }
