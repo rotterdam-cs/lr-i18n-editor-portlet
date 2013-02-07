a1b7e45c98e337956502d54b1db6e1eb52083f2c
Florencia Gadea <florencia@rotterdam-cs.com>
2013-02-07 15:52:25 -0300

    changes caused by using a different sdk and liferay version

--- docroot/WEB-INF/src/com/rcs/startup/LanguageStartupProcessor.java
+++ docroot/WEB-INF/src/com/rcs/startup/LanguageStartupProcessor.java
@@ -2,12 +2,16 @@ package com.rcs.startup;
 
 import com.rcs.service.model.MessageSource;
 import com.rcs.service.service.MessageSourceLocalServiceUtil;
+import com.rcs.service.service.impl.MessageSourceLocalServiceImpl;
 import com.rcs.common.utils.Importer;
 import com.rcs.common.utils.PortalLanguageResourcesUtil;
 
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.util.portlet.PortletProps;
@@ -20,7 +24,6 @@ import java.net.URL;
 import java.util.*;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 
 /**
  * Class that imports languages on startup
@@ -28,14 +31,15 @@ import org.apache.log4j.Logger;
  * @author flor
  */
 public class LanguageStartupProcessor extends SimpleAction {
-
-	protected static final Logger _logger = Logger.getLogger(LanguageStartupProcessor.class);
+	
+	private static Log _logger = LogFactoryUtil.getLog(LanguageStartupProcessor.class);
 
     @Override
     public void run(String[] ids) throws ActionException {
                     	
+    	_logger.error("Before Start language importing.");    	
         try {   
-        	if(StringUtils.equals("true", PortletProps.get("import.on.startup"))) {        		        
+        	if(StringUtils.equals("true", PropsUtil.get("import.on.startup"))) {        		        
 	        	_logger.info("Start language importing");
 	        	
 	        	Importer.importLanguages();
