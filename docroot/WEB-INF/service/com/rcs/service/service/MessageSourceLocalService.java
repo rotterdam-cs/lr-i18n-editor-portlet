c0b43194fc768dd573abce30cd1d6ef295bdf0cf
Florencia Gadea <florencia@rotterdam-cs.com>
2013-02-07 15:48:46 -0300

    changes caused by using a different sdk and liferay version

--- docroot/WEB-INF/service/com/rcs/service/service/MessageSourceLocalService.java
+++ docroot/WEB-INF/service/com/rcs/service/service/MessageSourceLocalService.java
@@ -19,8 +19,6 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.transaction.Isolation;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
-import com.liferay.portal.service.BaseLocalService;
-import com.liferay.portal.service.InvokableLocalService;
 import com.liferay.portal.service.PersistedModelLocalService;
 
 /**
@@ -38,8 +36,7 @@ import com.liferay.portal.service.PersistedModelLocalService;
  */
 @Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
 	PortalException.class, SystemException.class})
-public interface MessageSourceLocalService extends BaseLocalService,
-	InvokableLocalService, PersistedModelLocalService {
+public interface MessageSourceLocalService extends PersistedModelLocalService {
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
@@ -70,11 +67,10 @@ public interface MessageSourceLocalService extends BaseLocalService,
 	* Deletes the message source with the primary key from the database. Also notifies the appropriate model listeners.
 	*
 	* @param messageSourcePK the primary key of the message source
-	* @return the message source that was removed
 	* @throws PortalException if a message source with the primary key could not be found
 	* @throws SystemException if a system exception occurred
 	*/
-	public com.rcs.service.model.MessageSource deleteMessageSource(
+	public void deleteMessageSource(
 		com.rcs.service.service.persistence.MessageSourcePK messageSourcePK)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
@@ -83,14 +79,11 @@ public interface MessageSourceLocalService extends BaseLocalService,
 	* Deletes the message source from the database. Also notifies the appropriate model listeners.
 	*
 	* @param messageSource the message source
-	* @return the message source that was removed
 	* @throws SystemException if a system exception occurred
 	*/
-	public com.rcs.service.model.MessageSource deleteMessageSource(
+	public void deleteMessageSource(
 		com.rcs.service.model.MessageSource messageSource)
 		throws com.liferay.portal.kernel.exception.SystemException;
-
-	public com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery();
 
 	/**
 	* Performs a dynamic query on the database and returns the matching rows.
@@ -242,10 +235,6 @@ public interface MessageSourceLocalService extends BaseLocalService,
 	* @param beanIdentifier the Spring bean ID for this bean
 	*/
 	public void setBeanIdentifier(java.lang.String beanIdentifier);
-
-	public java.lang.Object invokeMethod(java.lang.String name,
-		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
-		throws java.lang.Throwable;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<java.lang.String> getMessageBundles()
