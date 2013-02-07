6c94bd19658b4057968985a56ca0d55ef6ec28b6
Florencia Gadea <florencia@rotterdam-cs.com>
2013-02-07 15:51:19 -0300

    changes caused by using a different sdk and liferay version

--- docroot/WEB-INF/src/com/rcs/service/model/impl/MessageSourceImpl.java
+++ docroot/WEB-INF/src/com/rcs/service/model/impl/MessageSourceImpl.java
@@ -15,7 +15,7 @@
 package com.rcs.service.model.impl;
 
 /**
- * The extended model implementation for the MessageSource service. Represents a row in the &quot;resourceEditor_MessageSource&quot; database table, with each column mapped to a property of this class.
+ * The extended model implementation for the MessageSource service. Represents a row in the &quot;editor_MessageSource&quot; database table, with each column mapped to a property of this class.
  *
  * <p>
  * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.rcs.service.model.MessageSource} interface.
